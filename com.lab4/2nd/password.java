3de950994de82789fd33a4b5f3917a3c32e5cec1
Smit <smitpandit200713@gmail.com>
2026-02-07 01:53:37 +0530

    que 2nd lab 4 completed

--- com.lab4/2nd/password.java
+++ com.lab4/2nd/password.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-
 // We need to write the function to check the password entered is correct or not based on the 
 // following conditions.
 // a) It must have atleast one lower case character and one digit.
@@ -12,56 +10,53 @@ import java.util.Scanner;
 // 123sd123 : acceptable, as not immediate pattern
 // adfasdsdf : not acceptable, as no digits
 // Aasdfasd12: not acceptable, as have uppercase character
-
 public class password {
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         char m;
-        int k=0;
-        boolean lower=false,digit=false;
+        boolean lower = false, digit = false;
         System.out.print("Enter the password : ");
         String pass = s.nextLine();
-        if(pass.length()>=5 && pass.length()<=12){
+        if (pass.length() >= 5 && pass.length() <= 12) {
             for (int i = 0; i < pass.length(); i++) {
                 m = pass.charAt(i);
                 //lowercase check
-                if((Character.isLowerCase(m))){
+                if ((Character.isLowerCase(m))) {
                     lower = true;
                 }
                 //special check
-                if(!Character.isLetterOrDigit(m)){
+                if (!Character.isLetterOrDigit(m)) {
                     System.out.println(" Not acceptable, as it have an special character");
                     return;
                 }
                 //upper case check
-                if(Character.isUpperCase(m)){
+                if (Character.isUpperCase(m)) {
                     System.out.println(" Not acceptable, as it have an uppercase character");
                     return;
                 }
                 //digit
-                if(String.valueOf(m).matches("[0-9]")){
+                if (String.valueOf(m).matches("[0-9]")) {
                     digit = true;
                 }
                 //immediate pattern
-                if(i<(pass.length()-2) && (pass.charAt(i)==pass.charAt(i+1))){
+                if (i < (pass.length() - 2) && (pass.charAt(i) == pass.charAt(i + 1))) {
                     System.out.println("Not acceptable, as it has a pattern");
                     return;
                 }
                 //repeting pattern
                 if (i < pass.length() - 3 && pass.substring(i, i + 2).equals(pass.substring(i + 2, i + 4))) {
-                System.out.println("Not acceptable, repeating pattern");
-                return;
+                    System.out.println("Not acceptable, repeating pattern");
+                    return;
+                }
             }
-            }
-            if(!digit){
+            if (!digit) {
                 System.out.println("Not acceptable, as no digits");
                 return;
             }
-            if(!lower){
+            if (!lower) {
                 System.out.println(" Not acceptable, as it doesn't have an lower character");
-                return;
-            }
-            else{
+            } else {
                 System.out.println("It works");
             }
         }
