8331e504556a8da648132f3d46d18d1e84536305
Smit <smitpandit200713@gmail.com>
2026-02-07 01:00:38 +0530

    initial commit

---
+++ com.lab4/2nd/password.java
@@ -0,0 +1,15 @@
+// We need to write the function to check the password entered is correct or not based on the 
+// following conditions.
+// a) It must have atleast one lower case character and one digit.
+// b) It must not have any Upper case characters and any special characters
+// c) length should be b/w 5-12.
+// d) It should not have any same immediate patterns like
+// abcanan1 : not acceptable coz of an an pattern
+// abc11se: not acceptable, coz of pattern 11
+// 123sd123 : acceptable, as not immediate pattern
+// adfasdsdf : not acceptable, as no digits
+// Aasdfasd12: not acceptable, as have uppercase character
+
+public class password {
+    
+}
